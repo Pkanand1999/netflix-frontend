acda45c00069dfcf4f2c8d939dcc38351a8df8f6
Pkanand1999 <singhisking810267@gmail.com>
2023-07-01 21:34:08 +0530

    player

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Netflix from './pages/Netflix';
+import Player from './pages/Player';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/player" element={<Player />} />
           <Route path="/" element={<Netflix />} />
         </Routes>
       </BrowserRouter>
---
+++ Netflix/src/pages/Player.jsx
@@ -0,0 +1,43 @@
+
+import styled from "styled-components";
+import { BsArrowLeft } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+export default function Player({videoId}) {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <div className="player">
+        <div className="back">
+          <BsArrowLeft onClick={() => navigate(-1)} />
+        </div>
+        <iframe  
+        src={`https://www.youtube.com/embed/${videoId}`}
+        allowfullscreen
+        />
+      </div>
+      
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .player {
+    width: 100vw;
+    height: 100vh;
+    .back {
+      position: absolute;
+      padding: 2rem;
+      z-index: 1;
+      svg {
+        font-size: 3rem;
+        cursor: pointer;
+      }
+    }
+    iframe {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+`;
